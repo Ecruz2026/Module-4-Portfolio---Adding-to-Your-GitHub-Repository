2222bb8cb9d6e090412e7f1f14cba7440e4a285f
Ecruz2026 <ecruz9@my.nl.edu>
2024-10-17 18:28:08 -0500

    Add files via upload

---
+++ pirate EC.py
@@ -0,0 +1,12 @@
+# Emmanuel Cruz
+
+# Oct 17, 2024
+
+
+
+greeting = input("Hello, possible pirate! What's the password?")
+if greeting in ("Arrr!"):
+   print("Go away, pirate.")
+
+elif greeting in ("Capitan"):
+   print("Greetings, hater of pirates!")
---
+++ time2 EC.py
@@ -0,0 +1,14 @@
+# Emmanuel Cruz
+
+# Oct 17, 2024
+
+
+
+
+str_time = input("What time is it now?")
+str_wait_time = input("What is the number of hours you want to wait?")
+time = int(str_time)
+wait_time = int(str_wait_time)
+
+time_when_alarm_go_off = time + wait_time
+print(time_when_alarm_go_off)
